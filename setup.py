ddd14976cb488f467bac66c3a285849d2eceea24
Oreoluwa Babarinsa <oreoluwa@babarinsa.me>
2016-10-16 13:25:14 -0700

    Renamed Ringbuffer.py to ringbuffer.py. Also finished the setup.py script

--- setup.py
+++ setup.py
@@ -1,6 +1,21 @@
 #! /usr/bin/env python3
 
+# We need this to define our package
 from setuptools import setup
+
+# We need to know the version to backfill some dependencies
+from sys import version_info, exit
+# Define our list of installation dependencies
+DEPENDS = ["zodb"]
+
+# If we're at version less than 3.4 - fail
+if version_info[0] < 3 or version_info[1] < 4:
+    exit("Unsupported version of Python. Minimum version for the Ingest SDK is 3.4")
+
+# If we're at version 3.4, backfill the typing library
+elif version_info[1] == 4:
+    DEPENDS.append("typing")
+    
 
 setup(
     name='snowflake_ingest',
@@ -20,5 +35,5 @@ setup(
         "Topic :: Database"
     ],
     # Now we describe the dependencies
-    install_requires=['zodb'],
+    install_requires=DEPENDS,
 )
