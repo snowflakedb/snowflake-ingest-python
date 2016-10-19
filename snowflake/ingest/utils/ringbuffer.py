5e39f4e9afe8146b2c6e61ef5a9aef320e8bda4d
Oreoluwa Babarinsa <oreoluwa@babarinsa.me>
2016-10-18 18:22:37 -0700

    Ringbuffer implementation efforts

--- snowflake/ingest/utils/ringbuffer.py
+++ snowflake/ingest/utils/ringbuffer.py
@@ -1,3 +1,9 @@
 """
-ringbuffer.py
+ringbuffer.py - This module implements a generic ringbuffer
 """
+
+# Type information makes everything better
+from typing import Generic
+
+class RingBuffer(object):
+    """Ring Buffer class"""
