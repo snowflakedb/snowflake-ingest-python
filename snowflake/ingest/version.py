4026b08935417b5ebef51d4a2a2de093d1699e05
ankit-bhatnagar167 <ankit.bhatnagar@snowflake.com>
2020-03-04 10:55:52 -0800

    Modified copyright year to current

--- snowflake/ingest/version.py
+++ snowflake/ingest/version.py
@@ -1,3 +1,3 @@
-# Copyright (c) 2012-2019 Snowflake Computing Inc. All rights reserved.
+# Copyright (c) 2012-2020 Snowflake Computing Inc. All rights reserved.
 # Update this for the versions
 __version__ = '1.0.2'
