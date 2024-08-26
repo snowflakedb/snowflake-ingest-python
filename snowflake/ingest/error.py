2bd24dd74607333b7b51aa272f70fa75072d2167
Xin Huang <xin.huang@snowflake.com>
2024-08-26 10:24:01 +0200

    Update error.py

--- snowflake/ingest/error.py
+++ snowflake/ingest/error.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2012-2023 Snowflake Computing Inc. All rights reserved.
+# Copyright (c) 2012-2024 Snowflake Computing Inc. All rights reserved.
 from requests import Response
 
 class IngestResponseError(Exception):
@@ -15,13 +15,17 @@ class IngestResponseError(Exception):
                                                                 response.reason)
             return
 
-        self.code = json_body[u'code']
-        self.success = json_body[u'success']
-        self._raw_message = json_body[u'message']
-        self.data = json_body[u'data']
-        self.message = 'Http Error: {}, Vender Code: {}, Message: {}'\
-            .format(self.http_error_code, self.code, self._raw_message)
-
+        try:
+            self.code = json_body[u'code']
+            self.success = json_body[u'success']
+            self._raw_message = json_body[u'message']
+            self.data = json_body[u'data']
+            self.message = 'Http Error: {}, Vender Code: {}, Message: {}' \
+                .format(self.http_error_code, self.code, self._raw_message)
+        except KeyError:
+            self.message = 'Http Error: {}, Message: {}, Body: {}'.format(self.http_error_code,
+                                                                          response.reason,
+                                                                          json_body)
         return
 
     def __str__(self):
